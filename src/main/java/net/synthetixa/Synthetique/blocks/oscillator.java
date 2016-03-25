3d05aef5034ebcdb25360308f4f24f54a84b502e
synthetixa <nucleria19@gmail.com>
2016-03-25 18:28:31 -0400

    Added blockstates (LFO, Audio, push) to oscillator. Switch with right click. Only audio is functional.

--- src/main/java/net/synthetixa/Synthetique/blocks/oscillator.java
+++ src/main/java/net/synthetixa/Synthetique/blocks/oscillator.java
@@ -1,21 +1,19 @@
 package net.synthetixa.Synthetique.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -23,71 +21,26 @@ import net.minecraft.world.World;
 
 public class oscillator extends Block implements ITileEntityProvider{
 
-    public static final PropertyDirection FACING;
-    public static final PropertyBool ISAUDIOOSC = PropertyBool.create("isAudioOsc");
+    public static final PropertyInteger TYPE = PropertyInteger.create("type", 0, 2);// 0 = audio, 1 = lfo, 2 = push
 
     public oscillator(Material material, String unlocalizedName) {
         super(material);
         this.setCreativeTab(CreativeTabs.tabRedstone);
         this.setHardness(1.0F);
         this.setUnlocalizedName(unlocalizedName);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-        this.setDefaultState(this.blockState.getBaseState().withProperty(ISAUDIOOSC, true));
-    }
-
-    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-        this.setDefaultFacing(worldIn, pos, state);
-    }
-
-    private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state) {
-        if(!worldIn.isRemote) {
-            IBlockState iblockstate = worldIn.getBlockState(pos.north());
-            IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
-            IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
-            IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
-            EnumFacing enumfacing = (EnumFacing) state.getValue(FACING);
-            if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock()) {
-                enumfacing = EnumFacing.SOUTH;
-            } else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock()) {
-                enumfacing = EnumFacing.NORTH;
-            } else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock()) {
-                enumfacing = EnumFacing.EAST;
-            } else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock()) {
-                enumfacing = EnumFacing.WEST;
-            }
-
-            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
-        }
-
-    }
-
-    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
-    }
-
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(TYPE, 0));
     }
 
     public IBlockState getStateFromMeta(int meta) {
-        EnumFacing enumfacing = EnumFacing.getFront(meta);
-        if(enumfacing.getAxis() == EnumFacing.Axis.Y) {
-            enumfacing = EnumFacing.NORTH;
-        }
-
-        return this.getDefaultState().withProperty(FACING, enumfacing);
+        return getDefaultState().withProperty(TYPE, Integer.valueOf(meta));
     }
 
-    public int getMetaFromState(IBlockState state) {
-        return ((EnumFacing)state.getValue(FACING)).getIndex();
+    public int getMetaFromState(IBlockState state){
+        return ((Integer)state.getValue(TYPE).intValue());
     }
 
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[]{FACING});
-    }
-
-    static {
-        FACING = BlockHorizontal.FACING;
+        return new BlockStateContainer(this, new IProperty[]{TYPE});
     }
 
     @Override
@@ -97,18 +50,6 @@ public class oscillator extends Block implements ITileEntityProvider{
 
     public boolean canConnectRedstone(IBlockAccess world, BlockPos pos, EnumFacing side)
     {
-        if (side == null) return false;
-        if (side == EnumFacing.UP || side == EnumFacing.DOWN) return false;
-        if (side == EnumFacing.NORTH) return false;
-
-        // we can connect to three of the four side faces - if the block is facing north, then we can
-        //  connect to WEST, SOUTH, or EAST.
-
-        EnumFacing whichFaceOfOsc = side.getOpposite();
-        IBlockState blockState = world.getBlockState(pos);
-        EnumFacing blockFacingDirection = (EnumFacing)blockState.getValue(FACING);
-
-        if (whichFaceOfOsc == blockFacingDirection) return false;
         return true;
     }
 
@@ -128,7 +69,7 @@ public class oscillator extends Block implements ITileEntityProvider{
         Block blockUp = worldIn.getBlockState(upPos).getBlock();
         Block blockDown = worldIn.getBlockState(downPos).getBlock();
 
-        if (ISAUDIOOSC.equals(true)){
+        if (worldIn.getBlockState(pos).getValue(TYPE).equals(0)){
             if(worldIn.isBlockPowered(pos)){
 
                 System.out.println("Oscillator is audio.");
@@ -153,23 +94,35 @@ public class oscillator extends Block implements ITileEntityProvider{
                 }
             }
         }
-        else if(ISAUDIOOSC.equals(false)){
+        else if(worldIn.getBlockState(pos).getValue(TYPE).equals(1)){
             if(worldIn.isBlockPowered(pos)) {
-                System.out.println("Oscillator is non-audio.");
+                System.out.println("Oscillator is LFO.");
+            }
+        }
+        else if(worldIn.getBlockState(pos).getValue(TYPE).equals(2)){
+            if(worldIn.isBlockPowered(pos)){
+                System.out.println("Oscillator is push.");
             }
         }
 
     }
 
-    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
 
-        if(worldIn.getBlockState(pos).getValue(ISAUDIOOSC).equals(true)){
-            worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(ISAUDIOOSC, false));
+        if(state.getValue(TYPE).equals(0)){
+            System.out.println("Switching oscillator state from audio to LFO.");
+            worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(TYPE, 1));
         }
-        else if(worldIn.getBlockState(pos).getValue(ISAUDIOOSC).equals(false)){
-            worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(ISAUDIOOSC, true));
+        else if(state.getValue(TYPE).equals(1)){
+            System.out.println("Switching oscillator state from LFO to push.");
+            worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(TYPE, 2));
+        }
+        else if(state.getValue(TYPE).equals(2)) {
+            System.out.println("Switching oscillator state from push to audio.");
+            worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(TYPE, 0));
         }
 
+        return false;
     }
 
 }
