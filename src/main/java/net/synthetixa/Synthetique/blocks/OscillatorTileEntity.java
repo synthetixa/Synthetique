c1fa97f1e96b6d00353a8e16d36331bfe03ac4fc
synthetixa <nucleria19@gmail.com>
2016-04-03 15:46:38 -0400

    Multiple oscillators, multiple pitches. Added back state saving. Going to add different tones for different oscillators and more uses.

--- src/main/java/net/synthetixa/Synthetique/blocks/OscillatorTileEntity.java
+++ src/main/java/net/synthetixa/Synthetique/blocks/OscillatorTileEntity.java
@@ -7,20 +7,35 @@ import net.minecraft.util.ITickable;
 public class OscillatorTileEntity extends TileEntity implements ITickable
 {
 
+    public float pitch = 0.1F;
+
     @Override
     public void update() {
 
     }
 
+    public void changePitch() {
+
+        worldObj.markChunkDirty(pos, this);
+        if (pitch == 2.0F) {
+            pitch = 0.1F;
+        } else {
+            pitch = pitch + 0.1F;
+        }
+    }
+
     @Override
     public void writeToNBT(NBTTagCompound compound){
         super.writeToNBT(compound);
-        float pitch = oscillator.getPitch();
+
+        compound.setFloat("pitch", pitch);
     }
 
     @Override
     public void readFromNBT(NBTTagCompound compound){
         super.readFromNBT(compound);
+
+        pitch = compound.getFloat("pitch");
     }
 
 }
--- src/main/java/net/synthetixa/Synthetique/blocks/oscillator.java
+++ src/main/java/net/synthetixa/Synthetique/blocks/oscillator.java
@@ -3,12 +3,10 @@ package net.synthetixa.Synthetique.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
@@ -16,19 +14,15 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.synthetixa.Synthetique.items.synthItems;
 
-import java.util.List;
-
 public class oscillator extends Block implements ITileEntityProvider{
 
     public static final PropertyInteger TYPE = PropertyInteger.create("type", 0, 2); // 0 = audio, 1 = lfo, 2 = push
-    public static float pitch;
 
     public oscillator(Material material, String unlocalizedName) {
         super(material);
@@ -43,11 +37,11 @@ public class oscillator extends Block implements ITileEntityProvider{
     }
 
     public int getMetaFromState(IBlockState state){
-        return ((Integer)state.getValue(TYPE).intValue());
+        return state.getValue(TYPE).intValue();
     }
 
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[]{TYPE});
+        return new BlockStateContainer(this, TYPE);
     }
 
     @Override
@@ -62,50 +56,50 @@ public class oscillator extends Block implements ITileEntityProvider{
 
     public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock) {
 
-        BlockPos northPos = pos.north();
-        BlockPos southPos = pos.south();
-        BlockPos westPos = pos.west();
-        BlockPos eastPos = pos.east();
-        BlockPos upPos = pos.up();
-        BlockPos downPos = pos.down();
+        TileEntity tileentity = worldIn.getTileEntity(pos);
 
-        Block blockNorth = worldIn.getBlockState(northPos).getBlock();
-        Block blockSouth = worldIn.getBlockState(southPos).getBlock();
-        Block blockWest = worldIn.getBlockState(westPos).getBlock();
-        Block blockEast = worldIn.getBlockState(eastPos).getBlock();
-        Block blockUp = worldIn.getBlockState(upPos).getBlock();
-        Block blockDown = worldIn.getBlockState(downPos).getBlock();
+        if (tileentity instanceof TileEntity) {
 
-        if (worldIn.getBlockState(pos).getValue(TYPE).equals(0)){
-            if(worldIn.isBlockPowered(pos)){
+            OscillatorTileEntity oscillatorTileEntity = (OscillatorTileEntity) tileentity;
 
-                if(blockNorth.equals(synthBlocks.speaker)){
-                    worldIn.playSound((EntityPlayer)null, northPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, pitch);
-                }
-                else if(blockSouth.equals(synthBlocks.speaker)){
-                    worldIn.playSound((EntityPlayer)null, southPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, pitch);
-                }
-                else if(blockWest.equals(synthBlocks.speaker)){
-                    worldIn.playSound((EntityPlayer)null, westPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, pitch);
-                }
-                else if(blockEast.equals(synthBlocks.speaker)){
-                    worldIn.playSound((EntityPlayer)null, eastPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, pitch);
-                 }
-                else if(blockUp.equals(synthBlocks.speaker)){
-                    worldIn.playSound((EntityPlayer)null, upPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, pitch);
-                }
-                else if(blockDown.equals(synthBlocks.speaker)){
-                    worldIn.playSound((EntityPlayer)null, downPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, pitch);
-                }
-            }
-        }
-        else if(worldIn.getBlockState(pos).getValue(TYPE).equals(1)){
-            if(worldIn.isBlockPowered(pos)) {
-            }
-        }
-        else if(worldIn.getBlockState(pos).getValue(TYPE).equals(2)){
-            if(worldIn.isBlockPowered(pos)){
+            BlockPos northPos = pos.north();
+            BlockPos southPos = pos.south();
+            BlockPos westPos = pos.west();
+            BlockPos eastPos = pos.east();
+            BlockPos upPos = pos.up();
+            BlockPos downPos = pos.down();
 
+            Block blockNorth = worldIn.getBlockState(northPos).getBlock();
+            Block blockSouth = worldIn.getBlockState(southPos).getBlock();
+            Block blockWest = worldIn.getBlockState(westPos).getBlock();
+            Block blockEast = worldIn.getBlockState(eastPos).getBlock();
+            Block blockUp = worldIn.getBlockState(upPos).getBlock();
+            Block blockDown = worldIn.getBlockState(downPos).getBlock();
+
+            if (worldIn.getBlockState(pos).getValue(TYPE).equals(0)) {
+                if (worldIn.isBlockPowered(pos)) {
+
+                    if (blockNorth.equals(synthBlocks.speaker)) {
+                        worldIn.playSound(null, northPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, oscillatorTileEntity.pitch);
+                    } else if (blockSouth.equals(synthBlocks.speaker)) {
+                        worldIn.playSound(null, southPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, oscillatorTileEntity.pitch);
+                    } else if (blockWest.equals(synthBlocks.speaker)) {
+                        worldIn.playSound(null, westPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, oscillatorTileEntity.pitch);
+                    } else if (blockEast.equals(synthBlocks.speaker)) {
+                        worldIn.playSound(null, eastPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, oscillatorTileEntity.pitch);
+                    } else if (blockUp.equals(synthBlocks.speaker)) {
+                        worldIn.playSound(null, upPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, oscillatorTileEntity.pitch);
+                    } else if (blockDown.equals(synthBlocks.speaker)) {
+                        worldIn.playSound(null, downPos, SoundEvents.block_note_harp, SoundCategory.BLOCKS, 3.0F, oscillatorTileEntity.pitch);
+                    }
+                }
+            } else if (worldIn.getBlockState(pos).getValue(TYPE).equals(1)) {
+                if (worldIn.isBlockPowered(pos)) {
+                }
+            } else if (worldIn.getBlockState(pos).getValue(TYPE).equals(2)) {
+                if (worldIn.isBlockPowered(pos)) {
+
+                }
             }
         }
 
@@ -114,7 +108,7 @@ public class oscillator extends Block implements ITileEntityProvider{
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
 
         if (heldItem != null) {
-            if (heldItem.areItemsEqual(new ItemStack(synthItems.screwdriver), heldItem)) {
+            if (ItemStack.areItemsEqual(new ItemStack(synthItems.screwdriver), heldItem)) {
                 if (state.getValue(TYPE).equals(0)) {
                     playerIn.addChatComponentMessage(new TextComponentString("Switching oscillator state from audio to LFO. (this won't make sound)"));
                     worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(TYPE, 1));
@@ -125,25 +119,20 @@ public class oscillator extends Block implements ITileEntityProvider{
                     playerIn.addChatComponentMessage(new TextComponentString("Switching oscillator state from push to audio. (this will make sound)"));
                     worldIn.setBlockState(pos, this.blockState.getBaseState().withProperty(TYPE, 0));
                 }
-            } else if (heldItem.areItemsEqual(new ItemStack(synthItems.tuner), heldItem)) {
-                if(pitch >= 2.0F){
-                    pitch = 0.0F;
-                }else{
-                    pitch = pitch + 0.1F;
+            } else if (ItemStack.areItemsEqual(new ItemStack(synthItems.tuner), heldItem)) {
+                TileEntity tileentity = worldIn.getTileEntity(pos);
+
+                if (tileentity instanceof TileEntity) {
+
+                    OscillatorTileEntity oscillatorTileEntity = (OscillatorTileEntity) tileentity;
+
+                    oscillatorTileEntity.changePitch();
                 }
             }
         }
 
         return false;
 
-    }
-
-    public static float getPitch(){
-        return pitch;
-    }
-
-    public static void setPitchFromNBT(float pitchF){
-        pitch = pitchF;
     }
 
 }
